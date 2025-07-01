a289e8f4b2019ffbddd3515804e64cfc6acd3a56
cweishimer <alphatec.viamao@gmail.com>
2025-06-30 23:19:16 -0300

    Projeto pronto

--- src/services/cart.js
+++ src/services/cart.js
@@ -1,6 +1,13 @@
 async function addItem(userCart, item) {
   userCart.push(item);
+}
+
+async function createItens(userCart, items) {
+  for (const item of items) {
+    await addItem(userCart, item);
+  }
   
+  return userCart;
 }
 
 async function deleteItem(userCart, name) {
@@ -15,14 +22,59 @@ async function deleteItem(userCart, name) {
 
 }
 
-async function removeItem(userCart, name) {
+async function removeItem(userCart, item) {
+ 
+  const indexFound = userCart.findIndex((p) => p.name === item.name);
+  
+  if (indexFound === -1) {
+    return console.log(`Item ${item.name} not found in the cart.`);
+  }
+
+  if(userCart[indexFound].quantity > 1) {
+    userCart[indexFound].quantity --;
+    return console.log(`Item ${userCart[indexFound].name} quantity decreased by 1.`);
+  }
+
+  if(userCart[indexFound].quantity === 1) {
+    return deleteItem(userCart, userCart[indexFound].name);
+  }
+
+}
+
+async function displayCart(userCart) {
+
+  if (userCart.length === 0) {
+    displayMessage("Your cart is empty.");
+    return;
+  }
+
+  console.log("Your cart items:\n");
+  console.log("  | Name        | Price x Quant  | SubTotal");
+  userCart.forEach((item, index) => {
+    
+    displayMessage(`${index + 1} | ${item.name} | R$ ${item.price.toFixed(2)} x ${item.quantity}    | SubTotal $${item.subTotal().toFixed(2)}`);
+
+  });
+
+  return displayMessage(await calculateTotal(userCart));
+
+}
+
+async function displayMessage(message) {
+  if (!message) {
+    message = "";
+  }
+  console.log("-----------------------------------------------");
+  console.log(message);
   
 }
 
 async function calculateTotal(userCart) {
   
-  return userCart.reduce((total, item) => total + item.subTotal(), 0);
-
+  if (userCart.length != 0) {
+    return displayMessage(`Total Cart: R$ ${userCart.reduce((total, item) => total + item.subTotal(), 0)}`);
+  }
+  
 }
 
-export { addItem, deleteItem, removeItem, calculateTotal };
+export { addItem, deleteItem, removeItem, calculateTotal, displayCart, displayMessage };
