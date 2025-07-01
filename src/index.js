ee50d7bd0c339173928ac79556fcfe915cc022ea
cweishimer <alphatec.viamao@gmail.com>
2025-06-30 23:18:45 -0300

    Peojeto cart shopee completo

--- src/index.js
+++ src/index.js
@@ -4,21 +4,25 @@ import * as cartService from "./services/cart.js";
 const myCart =[];
 const myWhislist = [];
 
-console.log(" 🛒 Welcome to the shopping cart application!");
+console.log(" 🛒 Welcome to the shopping cart application!\n\n");
 
-const item1 = await createItem("Apple", 1.99, 1);
-const item2 = await createItem("Orange", 1.75, 2);
-const item3 = await createItem("Banana", 1.00, 3);
-const item4 = await createItem("Grapes", 2.50, 1);
+
+const item1 = await createItem("Apple      ", 1.99, 1);
+const item2 = await createItem("Orange     ", 1.75, 3);
+const item3 = await createItem("Banana     ", 0.99, 1);
+const item4 = await createItem("Grapes     ", 2.50, 1);
+const item5 = await createItem("Watermelon ", 3.00, 2);
+
 
 await cartService.addItem(myCart, item1);
-await cartService.addItem(myWhislist, item2);
-await cartService.addItem(myCart, item3);
+await cartService.addItem(myCart, item2);
+await cartService.addItem(myCart, item3);   
+await cartService.addItem(myCart, item5);   
+await cartService.addItem(myWhislist, item3);   
 
-await cartService.deleteItem(myCart, item1 .name);
 
 
+//await cartService.deleteItem(myCart, "Banana");
 
-console.log("Your cart items:");
-console.log(cartService.calculateTotal(myCart));
+await cartService.displayCart(myCart);
 
